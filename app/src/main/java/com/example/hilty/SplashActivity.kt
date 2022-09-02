5170a0d19710899bb5481d629657c19aa1a5e9b5
Derrick-ivan <brianojiambo64@gmail.com>
2022-09-02 14:01:27 +0300

    my commit

---
+++ app/src/main/java/com/example/hilty/SplashActivity.kt
@@ -0,0 +1,49 @@
+package com.example.hilty
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val motionlayout=findViewById<MotionLayout>(R.id.motionLayout)
+        motionlayout.addTransitionListener(object : MotionLayout.TransitionListener{
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+
+
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+
+            }
+
+        })
+    }
+}
